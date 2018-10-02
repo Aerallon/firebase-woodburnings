4f62df848fd83c3ca7fee3bb6f337d8c3fb94e2d
smelnyk-va <smelnyk@vendasta.com>
2018-10-01 18:09:09 -0600

    Build out the home page component which will be used as the nav

---
+++ src/app/home-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.scss'],
+    selector: 'home-page'
+})
+
+export class HomePageComponent {
+  constructor() {
+  //  hi
+  }
+}
