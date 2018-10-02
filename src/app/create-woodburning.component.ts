9f1bdb5d360398b07d5d586bb1e5a5e1b808549b
smelnyk-va <smelnyk@vendasta.com>
2018-10-01 18:10:39 -0600

    Build out the create woodburning component - WIP

---
+++ src/app/create-woodburning.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+
+@Component({
+    templateUrl: './create-woodburning.component.html',
+    styleUrls: ['./create-woodburning.component.scss'],
+    selector: 'create-woodburning'
+})
+
+export class CreateWoodburningComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.form = this.formBuilder.group({
+      'title': ['', Validators.required],
+      'size': ['', Validators.required],
+      'material': ['', Validators.required],
+      'dateFinished': ['', Validators.required],
+      'totalTimeTakenMinutes': ['', Validators.required],
+      'totalTimeTakenHours': ['', Validators.required],
+      'imageUrl': ['', Validators.required],
+      'sharedOnline': ['', Validators.required],
+      'framed': ['', Validators.required],
+      'forSale': ['', Validators.required],
+      'sellingPrice': ['', Validators.required],
+      'sold': ['', Validators.required]
+    });
+  }
+}
