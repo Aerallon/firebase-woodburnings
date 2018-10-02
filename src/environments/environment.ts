1808b252a7f3bb8d4a7b673d9cfb3a1e84046a00
smelnyk-va <smelnyk@vendasta.com>
2018-10-01 21:06:55 -0600

    Update environment to have a collection name

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@ export const environment = {
   firebaseConfig: {
     apiKey: 'AIzaSyDSROWgmB9vVLzfDygZen2dAzjZHtklzaM',
     authDomain: 'my-woodburnings.firebaseapp.com',
-    databaseURL: 'https://my-woodburnings.firebaseio.com', // TODO double check this
+    databaseURL: 'https://my-woodburnings.firebaseio.com',
+    collectionName: 'my-woodburnings',
     storageBucket: 'my-woodburnings.appspot.com', // TODO double check this
     projectId: 'my-woodburnings'
   }
