8c6584a0a05e1df3a513e88dc07049309899b4b2
Smelnyk <smelnyk@vendasta.com>
2019-06-21 12:38:53 -0600

    Use mat grid list to show all the woodburnings currently for sale (IP)

--- src/app/for-sale/for-sale.component.ts
+++ src/app/for-sale/for-sale.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WoodburningDetails } from '../interfaces';
+import { WoodburningStoreService } from '../woodburning-store.service';
 
 
 @Component({
@@ -7,9 +10,15 @@ import { Component } from '@angular/core';
     selector: 'for-sale'
 })
 
-export class ForSaleComponent {
+export class ForSaleComponent implements OnInit {
 
-  constructor() {
+  public forSaleWoodburnings$: Observable<WoodburningDetails[]>;
+
+  constructor(private woodburningStoreService: WoodburningStoreService) {
     //
+  }
+
+  ngOnInit(): void {
+    this.forSaleWoodburnings$ = this.woodburningStoreService.listForSale();
   }
 }
