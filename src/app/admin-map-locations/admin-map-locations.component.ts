95cc0d35828e5eda9d6a66e8d34742352aea4a5c
Smelnyk <smelnyk@vendasta.com>
2019-06-22 13:55:12 -0600

    Create Admin Map Locations Component

---
+++ src/app/admin-map-locations/admin-map-locations.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './admin-map-locations.component.html',
+    styleUrls: ['./admin-map-locations.component.scss'],
+    selector: 'admin-map-locations'
+})
+
+export class AdminMapLocationsComponent {
+
+  constructor() {
+    //
+  }
+}
